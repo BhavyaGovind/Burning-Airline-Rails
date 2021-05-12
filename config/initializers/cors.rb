99089feb3e5978fda83ad02547bed57330744d82
mac rent <macrent@macs-MacBook-Air-2.local>
2021-05-12 14:41:58 +1000

    updated CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # This config jacked from rack-cors Github (Which see)
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001' ##this needs an explicit list of your URLs on heroku
+    origins 'https://localhost:3000', 'https://localhost:3001', 'https://burning-airlines-bcdk-react.herokuapp.com' ##this needs an explicit list of your URLs on heroku
     resource '*',
     :headers => :any,
     :methods => %i( get post put patch delete options head )
